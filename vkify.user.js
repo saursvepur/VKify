a134f073afb43d8f2082b0d5ccc3054f2d79d525
koke228666 <146654074+koke228666@users.noreply.github.com>
2025-01-25 19:25:35 +0300

    теперь громкость в AJAX плеере будет нормально меняться 2

--- vkify.user.js
+++ vkify.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VKify
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.4.1
 // @description  Дополнительные штуки-друюки для VKify
 // @author       koke228
 // @match        *://ovk.to/*
